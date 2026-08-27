c2ba9e0f53babdae9d20b6446faf39079d5e64db
MariyaTre <maschacevardina1@gmail.com>
2026-08-27 22:13:53 +0400

    Add folder click handling and opened folder state

--- src/App.jsx
+++ src/App.jsx
@@ -6,36 +6,51 @@ import ToolsButton from './assets/ToolsButton.png'
 import TimeButton from './assets/TimeButton.png'
 import folder from './assets/folder.png'
 import document from './assets/Document.png'
+import {useState} from 'react'
 
 function App() {
+
+    const [openedFolder, setOpenFolder] = useState('')
+
     const folders = [
         'Новая папка 1',
         'Новая папка 2',
         'Новая папка 3'
     ]
-const documents = [
-    'Новый документ 1',
-    'Новый документ 2',
-    'Новый документ 3'
-]
+    const documents = [
+        'Новый документ 1',
+        'Новый документ 2',
+        'Новый документ 3'
+    ]
+
+    function openFolder(folderName) {
+        console.log('Папка открыта', folderName);
+        setOpenFolder(folderName);
+    }
+
 
     return (
         <main className="desktop">
             <div className="workspace">
+                {openedFolder
+                    ? <p>Открыта папка: {openedFolder}</p>
+                    : <p>Рабочий стол</p>
+                }
                 {folders.map((folderName) => (
                     <div className="folder" key={folderName}>
                         <img className="folder-img"
                              src={folder}
-                             alt={folder}/>
+                             alt={folder}
+                             onClick={() => openFolder(folderName)}/>
                         <p className="folder-text">{folderName}</p>
                     </div>
                 ))}
-                {documents.map((folderName) => (
-                    <div className="document" key={folderName}>
+                {documents.map((documentName) => (
+                    <div className="document" key={documentName}>
                         <img className="document-img"
                              src={document}
                              alt={document}/>
-                        <p className="document-text">{folderName}</p>
+                        <p className="document-text">{documentName}</p>
                     </div>
                 ))}
             </div>
@@ -46,7 +61,8 @@ const documents = [
                          alt="logo-button"/>
                     <img className="home-button"
                          src={HomeButton}
-                         alt="home-button"/>
+                         alt="home-button"
+                         onClick={() => openFolder('')}/>
                 </div>
                 <div className="right-panel">
                     <img className="en-button"
