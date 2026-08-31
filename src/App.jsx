e4df08df608574d41ba7eefbc5618483c859836b
MariyaTre <maschacevardina1@gmail.com>
2026-08-31 17:07:34 +0400

    Added empty-folder handling, document windows with open and close functionality, and a Home button that closes all open windows.

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import {useState} from 'react'
 function App() {
 
     const [openedFolder, setOpenFolder] = useState('')
+    const [openedDocument, setOpenDocument] = useState('')
+
 
     const folders = [
         'Новая папка 1',
@@ -34,6 +36,17 @@ function App() {
         setOpenFolder(folderName);
     }
 
+    function openDocument(documentName) {
+        console.log('Документ открыт', documentName);
+        setOpenDocument(documentName);
+    }
+
+    function goHome() {
+        setOpenFolder('');
+        setOpenDocument('');
+    }
+
+
 
     return (
         <main className="desktop">
@@ -50,18 +63,37 @@ function App() {
                             </button>
                         </div>
                         <div className="folder-window-content">
-                            {folderContents[openedFolder].map((documentName) => (
-                                <div className="document" key={documentName}>
-                                    <img className="document-img"
-                                         src={document}
-                                         alt={document}/>
-                                    <p className="document-text">{documentName}</p>
-                                </div>
+                            {folderContents[openedFolder].length === 0
+                                ? <p className="folder-window-content-p">Папка пуста</p>
+                                : folderContents[openedFolder].map((documentName) => (
+                                    <div className="document" key={documentName}>
+                                        <img className="document-img"
+                                             src={document}
+                                             alt={document}
+                                             onClick={() => openDocument(documentName)}/>
+                                        <p className="document-text">{documentName}</p>
+                                    </div>
                                 ))}
                         </div>
                     </div>
                     : null
                 }
+                {openedDocument
+                    ? <div className="document-window">
+                        <div className="document-window-header">
+                            <span className="document-window-title">
+                                {openedDocument}
+                            </span>
+                            <button className="document-window-header-button"
+                            onClick={() => openDocument('')}>
+                                ×
+                            </button>
+                        </div>
+                        <div className="document-window-content">
+
+                        </div>
+                    </div>
+                    : null}
                 {folders.map((folderName) => (
                     <div className="folder" key={folderName}>
                         <img className="folder-img"
@@ -75,7 +107,8 @@ function App() {
                     <div className="document" key={documentName}>
                         <img className="document-img"
                              src={document}
-                             alt={document}/>
+                             alt={document}
+                             onClick={() => openDocument(documentName)}/>
                         <p className="document-text">{documentName}</p>
                     </div>
                 ))}
@@ -88,7 +121,7 @@ function App() {
                     <img className="home-button"
                          src={HomeButton}
                          alt="home-button"
-                         onClick={() => openFolder('')}/>
+                         onClick={goHome}/>
                 </div>
                 <div className="right-panel">
                     <img className="en-button"
